ce4d678c27bbac3dd3aafd154c31e2d3420b90c8
JodyWilmes <65562004+JodyWilmes@users.noreply.github.com>
2023-02-06 12:00:47 +0100

    Camera Rotation
    Looking around with the camera by using your mouse working

---
+++ Assets/_scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private Transform _eyes;
+    [SerializeField] private float _sensitivity;
+    [Range (-90.0f, 0.0f)]
+    [SerializeField] private float _camLimitMin;
+    [Range (0.0f, 90.0f)]
+    [SerializeField] private float _camLimitMax;
+
+    private float _camAngle = 0.0f;
+
+    void Update()
+    {
+        RotateEyes();
+    }
+
+    private void RotateEyes()
+    {
+        float yMouse = Input.GetAxis("Mouse Y") * _sensitivity * Time.deltaTime;
+        _camAngle -= yMouse;
+        _camAngle = Mathf.Clamp (_camAngle, _camLimitMin, _camLimitMax);
+        _eyes.localRotation = Quaternion.Euler(_camAngle, 0, 0);
+    }
+
+    private void RotateBody()
+    {
+        float xMouse = Input.GetAxis("Mouse X") * _sensitivity * Time.deltaTime;
+        Transform.Rotate(Vector3.up * xMouse);
+    }
+}
